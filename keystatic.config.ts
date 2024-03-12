075517577f918935baa59acf67bb14fbacd892d2
Levi Schouten <levi.schouten.werk@gmail.com>
2024-03-12 10:16:20 +0100

    Change storage kind from local to cloud

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,7 +2,7 @@ import { config, fields, collection, singleton } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: "local",
+    kind: "cloud",
   },
   cloud: {
     project: "levi-schouten/wansuda-nl",
